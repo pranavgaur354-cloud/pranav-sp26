d6086134956df3de1b3d0bcca1bb29c1378e36de
pranavgaur354-cloud <pranavgaur354@gmail.com>
2026-04-24 12:40:47 +0530

    hw03 COMPLETE

---
+++ hw03/src/IntList.java
@@ -0,0 +1,89 @@
+public class IntList {
+    int first;
+    IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    /** Return the size of the list using... recursion! */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    /** Return the size of the list using no recursion! */
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
+    /** Returns the ith item of this IntList. */
+    public int get(int i) {
+        if (i == 0) {
+            return first;
+        }
+        return rest.get(i - 1);
+    }
+
+    /**
+     * Returns an IntList identical to L, but with
+     * each element incremented by x. Modifies the original list.
+     * You are not allowed to use "new" in this method.
+     */
+    public static IntList incrRecursiveDestructive(IntList L, int x) {
+        // TODO: Fill in this code
+
+        if(L.rest==null){
+            L.first= L.first + x;
+            return L;
+        }
+        L.first= L.first + x;
+        incrRecursiveDestructive(L.rest,x);
+        return L;
+
+    }
+
+
+    /*
+     * =================================================================
+     * OPTIONAL METHODS
+     * =================================================================
+     */
+
+    /**
+     * Returns the sum of all elements in the IntList.
+     */
+    public int sum() {
+
+        if (rest == null){
+            return first;
+    }
+        return rest.sum() + first;
+    }
+
+    /**
+     * Destructively adds x to the end of the list.
+     */
+    public void addLast(int x) {
+        // Optional: Fill in this code
+    }
+
+    /**
+     * Destructively adds x to the front of this IntList.
+     * This is a bit tricky to implement. The standard way to do this would be
+     * to return a new IntList, but for practice, this implementation should
+     * be destructive.
+     */
+    public void addFirst(int x) {
+        // Optional: Fill in this code
+    }
+}
