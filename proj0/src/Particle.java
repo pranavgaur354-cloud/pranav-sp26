25b076fc42f184f7e8cad898950f253ee2d860f5
pranavgaur354-cloud <pranavgaur354@gmail.com>
2026-04-24 14:32:39 +0530

    [task 0-1] code run and color method COMPLETE

---
+++ proj0/src/Particle.java
@@ -0,0 +1,68 @@
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.awt.*;
+import java.util.Map;
+
+public class    Particle {
+    public ParticleFlavor flavor;
+    public int lifespan;
+
+    public static final int PLANT_LIFESPAN = 150;
+    public static final int FLOWER_LIFESPAN = 75;
+    public static final int FIRE_LIFESPAN = 10;
+    public static final Map<ParticleFlavor, Integer> LIFESPANS =
+            Map.of(ParticleFlavor.FLOWER, FLOWER_LIFESPAN,
+                   ParticleFlavor.PLANT, PLANT_LIFESPAN,
+                   ParticleFlavor.FIRE, FIRE_LIFESPAN);
+
+    public Particle(ParticleFlavor flavor) {
+        this.flavor = flavor;
+        lifespan = -1;
+    }
+
+    public Color color() {
+        if (flavor == ParticleFlavor.EMPTY) {
+            return Color.BLACK;
+        }
+        else if (flavor == ParticleFlavor.SAND) {
+            return Color.YELLOW;
+        }
+        else if (flavor == ParticleFlavor.BARRIER) {
+            return Color.GRAY;
+        }
+        else if (flavor == ParticleFlavor.  WATER) {
+            return Color.BLUE;
+        }
+        else if (flavor == ParticleFlavor.FOUNTAIN) {
+            return Color.CYAN;
+        }
+        else if (flavor == ParticleFlavor.PLANT) {
+            return new Color(0,255,0) ;
+        }
+        else if (flavor == ParticleFlavor.FIRE) {
+            return new Color(255,0,0);
+        }
+        else if (flavor == ParticleFlavor.FLOWER) {
+            return new Color(255,141,161);
+        }
+        return Color.GRAY;
+    }
+
+    public void moveInto(Particle other) {
+    }
+
+    public void fall(Map<Direction, Particle> neighbors) {
+    }
+
+    public void flow(Map<Direction, Particle> neighbors) {
+    }
+
+    public void grow(Map<Direction, Particle> neighbors) {
+    }
+
+    public void burn(Map<Direction, Particle> neighbors) {
+    }
+
+    public void action(Map<Direction, Particle> neighbors) {
+    }
+}
